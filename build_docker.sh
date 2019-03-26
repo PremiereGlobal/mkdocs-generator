bf254ba8607cea2bf1cfcc4b2f7cd711b25da3e8
Chris Bartlett <bartlettc@gmail.com>
2019-03-26 00:29:34 -0600

    working on build 4

--- build_docker.sh
+++ build_docker.sh
@@ -5,6 +5,7 @@ TAG=${1:-"dev"}
 PUBLISH=${2:-""}
 
 if [ "${PUBLISH}" == "true" ]; then
+  echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
   docker push ${REPO}:${TAG}
 else
   docker build -t ${REPO}:${TAG} ./
